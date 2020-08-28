28c37c9fa3551a08e2702e2851b4c14aaa0aa2a8
beardedmurph <darrenkmmurphy@gmail.com>
2020-08-28 22:14:42 +0100

    Fourth commit of modules repo

--- services/webserver-cluster/outputs.tf
+++ services/webserver-cluster/outputs.tf
@@ -7,8 +7,3 @@ output "clb_dns_name" {
   value       = aws_elb.example.dns_name
   description = "The domain name of the load balancer"
 }
-
-output "instance_ip_addr" {
-  value       = aws_instance.server.private_ip
-  description = "The private IP address of the main server instance."
-}
