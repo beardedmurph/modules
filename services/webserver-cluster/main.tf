caebdaeb0492774ce605215579fa93bbd0e28353
beardedmurph <darrenkmmurphy@gmail.com>
2020-08-29 17:07:53 +0100

    Added autoscaling schedule

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -43,12 +43,31 @@ resource "aws_autoscaling_group" "example" {
   load_balancers    = [aws_elb.example.name]
   health_check_type = "ELB"
 
+  resource "aws_autoscaling_schedule" "scale_out_business_hours" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name  = "scale-out-during-business-hours"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 10
+  recurrence             = "0 9 * * *"
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name  = "scale-in-at-night"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 2
+  recurrence             = "0 17 * * *"
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
+
   tag {
     key                 = "Name"
     value               = "${var.cluster_name}"
     propagate_at_launch = true
   }
-  
+
   dynamic "tag" {
     for_each = var.custom_tags
     content {
